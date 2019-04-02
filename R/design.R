bc1cd0c731e148b1c8591f43953e9b8d0663a74a
tobiasmuetze <git@tobiasmuetze.com>
2019-04-02 13:20:55 +0200

    Introduced arguments in design_gsnb allowing for different timing of efficacy and futility analyses.

--- R/design.R
+++ R/design.R
@@ -114,7 +114,10 @@ add_stopping_prob <- function(x) {
 #' @param dispersion numeric; dispersion (shape) parameter of negative binomial distribution
 #' @param power numeric; target power of group sequential design
 #' @param timing numeric vector; 0 < \code{timing[1]} < ... < \code{timing[K]} = 1
-#' with \code{K} the number of analyses, i.e. (K-1) interim analyses and final analysis
+#' with \code{K} the number of analyses, i.e. (K-1) interim analyses and final analysis.
+#' When the timing of efficacy and futility analyses differ, timing should not be defined.
+#' Instead, the arguments \code{timing_eff} and \code{timing_fut} have to be used to specify
+#' the timing of the efficacy and futility analyses, respectively. 
 #' @param esf function; error spending function
 #' @param ratio_H0 numeric; positive number denoting the rate ratio \eqn{\mu_1/\mu_2}
 #' under the null hypothesis, i.e. the non-inferiority or superiority margin
@@ -133,7 +136,7 @@ add_stopping_prob <- function(x) {
 #' @param accrual_speed numeric; determines accrual speed; values larger than 1
 #' result in accrual slower than linear; values betwen 0 and 1 result in accrual 
 #' faster than linear.  
-#' @param ... further arguments to be passed to the error spending function
+#' @param ... further arguments. Will be passed to the error spending function.
 #' @return A list with class "gsnb" containing the following components:
 #' \item{rate1}{as input}
 #' \item{rate2}{as input}
@@ -198,6 +201,15 @@ add_stopping_prob <- function(x) {
 #'                    power = 0.8, timing = c(0.5, 1), esf = obrien,
 #'                    ratio_H0 = 1, sig_level = 0.025,
 #'                    followup_max = 0.5, random_ratio = 1)
+#'                    
+#' # Different timing for efficacy and futility analyses
+#'  design_gsnb(rate1 = 1, rate2 = 2, dispersion = 5,
+#'              power = 0.8, esf = obrien,
+#'              ratio_H0 = 1, sig_level = 0.025, study_period = 3.5,
+#'              accrual_period = 1.25, random_ratio = 1, futility = "binding",
+#'              esf_futility = pocock, 
+#'              timing_eff = c(0.8, 1),
+#'              timing_fut = c(0.2, 0.5, 1))                    
 #' @export
 design_gsnb <- function(rate1, rate2, dispersion, ratio_H0 = 1, random_ratio = 1, 
                         power, sig_level, timing, esf = obrien,
@@ -206,6 +218,22 @@ design_gsnb <- function(rate1, rate2, dispersion, ratio_H0 = 1, random_ratio = 1
                         accrual_period = NULL, followup_max = NULL, 
                         accrual_speed = 1, ...) {
   
+  # Read out separate input of timing for efficacy and futility stopping
+  timing_eff <- list(...)$timing_eff
+  timing_fut <- list(...)$timing_fut
+
+  # Error check for timing variables  
+  if (missing(timing) & !is.null(timing_eff) & !is.null(timing_fut)) {
+    timing <- sort(unique(c(timing_eff, timing_fut)))
+  } else if (missing(timing) & (is.null(timing_eff) | is.null(timing_fut))) {
+    stop("Argument 'timing' is missing and 'timing_eff' or 'timing_fut' is missing, too.")
+  } else {
+    if (!is.null(timing_eff) | !is.null(timing_fut)) {
+      warning("Argument 'timing' is specified; 'timing_eff' and 'timing_fut' will be overwritten.")
+    }
+    timing_eff <- timing_fut <- timing
+  }
+    
   isNull_fm <- is.null(followup_max)
   isNull_sp <- is.null(study_period)
   isNull_ap <- is.null(accrual_period)
@@ -219,17 +247,28 @@ design_gsnb <- function(rate1, rate2, dispersion, ratio_H0 = 1, random_ratio = 1
   x <- list(rate1 = rate1, rate2 = rate2, dispersion = dispersion, 
             ratio_H0 = ratio_H0, power = power, sig_level = sig_level, 
             timing = timing, random_ratio = random_ratio)
-  ## Efficacy spending
-  esf_eff_out <- esf(t = timing, 
+  # Efficacy spending
+  esf_eff_out <- esf(t = timing_eff, 
                      sig_level = sig_level, ...)
-  x$efficacy <- list(esf = esf, 
-                     spend = c(esf_eff_out[1], diff(esf_eff_out)))
-  ## Futility spending
+  spend_eff <- c(esf_eff_out[1], diff(esf_eff_out))
+  # Futility spending
   if (!is.null(futility)) {
-    esf_fut_out <- esf_futility(t = timing, sig_level = 1 - power, ...)
+    esf_fut_out <- esf_futility(t = timing_fut, sig_level = 1 - power, ...)
+    spend_fut <- c(esf_fut_out[1], diff(esf_fut_out))
+    df_fut <- data.frame(timing = timing_fut, spend_fut = spend_fut)
+    
+    df_eff <- data.frame(timing = timing_eff, spend_eff = spend_eff)
+    df_merge <- merge(df_eff, df_fut, all = TRUE)
+    df_merge[is.na(df_merge)] <- 0
+    
     x$futility <- list(esf = esf_futility,
-                       spend = c(esf_fut_out[1], diff(esf_fut_out)),
+                       spend = df_merge$spend_fut,
                        type = futility)
+    x$efficacy <- list(esf = esf, 
+                       spend = df_merge$spend_eff)
+  } else {
+    x$efficacy <- list(esf = esf, 
+                       spend = spend_eff)
   }
   x$ratio_H1 <- rate1 / rate2
   class(x) <- "gsnb"
@@ -238,14 +277,6 @@ design_gsnb <- function(rate1, rate2, dispersion, ratio_H0 = 1, random_ratio = 1
   
   # Add maximum information to x
   x <- add_maxinfo(x)
-  
-  # # Add stopping probabilities to x
-  # x <- add_stopping_prob(x)
-  # 
-  # Add power of fixed design to x
-  log_effect <- log(x$rate1 / x$rate2) - log(x$ratio_H0)
-  x$power_fix <- pnorm(qnorm(sig_level), mean = log_effect * sqrt(x$max_info))
-  
   
   # Calcualte the missing design characteristics
   # 1. Sample size in the case of a fixed followup.
@@ -304,6 +335,9 @@ design_gsnb <- function(rate1, rate2, dispersion, ratio_H0 = 1, random_ratio = 1
   class(x) <- "gsnb"
   # Add stopping probabilities to x
   x <- add_stopping_prob(x)
+  # Add power of fixed design to x
+  log_effect <- log(x$rate1 / x$rate2) - log(x$ratio_H0)
+  x$power_fix <- pnorm(qnorm(sig_level), mean = log_effect * sqrt(x$max_info))
   
   x
 }
